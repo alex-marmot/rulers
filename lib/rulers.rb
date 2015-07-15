11e98984b2d952993753b8764d0e20121b34e0c9
alex-marmot <alex.marmot@gmail.com>
2015-07-15 14:46:54 +0800

    chapter 2 finished

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,12 +1,30 @@
 require "rulers/version"
-require "rulers/array"
+require "rulers/routing"
 
 module Rulers
   class Application
     def call(env)
-      `echo debug > debug.txt`
-      [200, {'COntent-Type' => 'text/html'},
-      [[1, 2, 3].sum.to_s]]
+      if env["PATH_INFO"] == '/favicon.ico'
+        return [404, {"Content-Type" => "text/html"}, []]
+      end
+      if env["PATH_INFO"] == '/'
+        env["PATH_INFO"] = '/quotes/a_quote'
+      end
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+      [200, {'Content-Type' => 'text/html'},
+      [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
